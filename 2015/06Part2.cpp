ac71df104dd3ca3a4507731c452d5f900fee2f43
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2024-10-17 12:47:36 +0100

    Added 06Part2 and removed the set for better performance.

--- 2015/06Part2.cpp
+++ 2015/06Part2.cpp
@@ -1,20 +1,23 @@
 /*
---- Day 6: Probably a Fire Hazard ---
-Because your neighbors keep defeating you in the holiday house decorating contest year after year, you've decided to deploy one million lights in a 1000x1000 grid.
+--- Part Two ---
+You just finish implementing your winning light pattern when you realize you mistranslated Santa's message from Ancient Nordic Elvish.
 
-Furthermore, because you've been especially nice this year, Santa has mailed you instructions on how to display the ideal lighting configuration.
+The light grid you bought actually has individual brightness controls; each light can have a brightness of zero or more. The lights all start at zero.
 
-Lights in your grid are numbered from 0 to 999 in each direction; the lights at each corner are at 0,0, 0,999, 999,999, and 999,0. The instructions include whether to turn on, turn off, or toggle various inclusive ranges given as coordinate pairs. Each coordinate pair represents opposite corners of a rectangle, inclusive; a coordinate pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3 square. The lights all start turned off.
+The phrase turn on actually means that you should increase the brightness of those lights by 1.
 
-To defeat your neighbors this year, all you have to do is set up your lights by doing the instructions Santa sent you in order.
+The phrase turn off actually means that you should decrease the brightness of those lights by 1, to a minimum of zero.
+
+The phrase toggle actually means that you should increase the brightness of those lights by 2.
+
+What is the total brightness of all lights combined after following Santa's instructions?
 
 For example:
 
-turn on 0,0 through 999,999 would turn on (or leave on) every light.
-toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off the ones that were on, and turning on the ones that were off.
-turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
-After following the instructions, how many lights are lit?
+turn on 0,0 through 0,0 would increase the total brightness by 1.
+toggle 0,0 through 999,999 would increase the total brightness by 2000000.
 */
+#include <cstdint>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -104,7 +107,7 @@ int main(int argc, char **argv)
   char **chord_vec;
   char *Line = NULL;  
   // std::set<int> lit_lights_set;
-  static bool lights[LIGHT_COUNT] = {0}; 
+  static uint64_t lights[LIGHT_COUNT] = {0}; 
   while (1) {
     nread = getline(&Line, &n, fp);
     if (nread <= 0)
@@ -167,7 +170,8 @@ int main(int argc, char **argv)
       for (int x = ins.start.x; x <= ins.end.x; ++x) {
         for (int y = ins.start.y; y <= ins.end.y; ++y) {
            int cell = (x * WIDTH) + y;
-           lights[cell] = 0;
+          if (lights[cell] != 0) 
+            lights[cell] -= 1;
         }
       }
     } break;
@@ -175,7 +179,7 @@ int main(int argc, char **argv)
       for (int x = ins.start.x; x <= ins.end.x; ++x) {
         for (int y = ins.start.y; y <= ins.end.y; ++y) {
            int cell = (x * WIDTH) + y;
-           lights[cell] = 1;
+           lights[cell] += 1;
         }
       }      
     } break;
@@ -183,7 +187,7 @@ int main(int argc, char **argv)
       for (int x = ins.start.x; x <= ins.end.x; ++x) {
         for (int y = ins.start.y; y <= ins.end.y; ++y) {
            int cell = (x * WIDTH) + y;
-           lights[cell] = !lights[cell];
+           lights[cell] += 2;
         }
       }
     } break;
@@ -195,14 +199,13 @@ int main(int argc, char **argv)
     Line = NULL;
   }
 
-  size_t turned_on = 0, turned_off = 0; 
+  size_t brightness = 0;
   for (int y = 0; y < HEIGHT; ++y) {
     for (int cell = 0; cell < WIDTH; cell++) {
       int idx = (HEIGHT * y) + cell;
-      turned_on += lights[idx];
-      turned_off += !lights[idx];
+      brightness += lights[idx];
     }
   }
-  printf("Turned off: %zu Turned on: %zu\n", turned_off, turned_on);
+  printf("brightness: %zu\n", brightness);
   return (0);
 }
